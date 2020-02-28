7ff8a8bf2bc5120b617dd9afca3767f39f28ffe1
kleber <klebersurf123@gmail.com>
2020-02-28 20:32:10 -0300

    solucão do exercicios

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace cidades
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("mongaguá");
+            Console.WriteLine("itanhaem");
+            Console.WriteLine("peruibe");
+        }
+    }
+}
